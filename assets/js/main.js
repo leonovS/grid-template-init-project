db11b0176953564d08f1da7606d0ae401e0d2a58
LeonovS <leon19840709@gmail.com>
2021-11-15 21:35:15 +0300

    закончил с кнопкой меню

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 
-$(document).ready(function () {
+$(document).ready(() => {
 
   $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').stop(true, true) .slideToggle(500)
   });
 
 });
